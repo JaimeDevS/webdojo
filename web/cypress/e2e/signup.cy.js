9db81d765c375106d0827ec5f6423f7ac22b0943
JaimeDevS <jaime.dev91@gmail.com>
2026-06-05 21:18:44 -0300

    Criada API mock e testes com bruno

--- web/cypress/e2e/signup.cy.js
+++ web/cypress/e2e/signup.cy.js
@@ -2,12 +2,12 @@ describe('Cadastro', () => {
     beforeEach(() => {
         cy.goToSignup()
 
-        cy.intercept('POST', 'http://localhost:3333/api/users/register', {
-            statusCode: 201,
-            body: {
-                message: 'Usuário cadastrado com sucesso'
-            }
-        }).as('postSignup')
+        // cy.intercept('POST', 'http://localhost:3333/api/users/register', {
+        //     statusCode: 201,
+        //     body: {
+        //         message: 'Usuário cadastrado com sucesso'
+        //     }
+        // }).as('postSignup')
     }) 
 
     it('Deve cadastrar um novo usuáiro', () => {
@@ -17,7 +17,7 @@ describe('Cadastro', () => {
 
         cy.contains('button', 'Criar conta').click()
 
-        cy.wait('@postSignup')
+        //cy.wait('@postSignup')
 
         cy.contains('Conta criada com sucesso!')
             .should('be.visible')
