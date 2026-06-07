a7c424b80c85e7a43903e60e621eae7150f0e041
JaimeDevS <jaime.dev91@gmail.com>
2026-06-06 21:00:07 -0300

    Validando regras adicionais e Códigos HTTP

--- api/cypress/e2e/post.cy.js
+++ api/cypress/e2e/post.cy.js
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker'
 
 describe('POST /api/users/register', () => {
+
   it('deve cadastrar um novo usuário', () => {
 
     const user = {
@@ -12,11 +13,8 @@ describe('POST /api/users/register', () => {
     cy.api({
       method: 'POST',
       url: 'http://localhost:3333/api/users/register',
-      body: {
-        name: user.name,
-        email: user.email,
-        password: user.password
-      }
+      body: user,
+      failOnStatusCode: false
     }).then((response) => {
       expect(response.status).to.eq(201)
       //cy.log(JSON.stringify(response.body))
@@ -26,5 +24,73 @@ describe('POST /api/users/register', () => {
       expect(response.body.user.email).to.eq(user.email)
     })
 
+  })
+
+  it('Não deve cadastrar com email duplicado', () => {
+
+    const user = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: 'pwd123'
+    }
+
+    cy.postUser(user).then((response) => {
+      expect(response.status).to.eq(201)
+    })
+
+    cy.postUser(user).then((response) => {
+      expect(response.status).to.eq(409)
+      expect(response.body.error).to.eq('A user with this email address already exists.')
+    })
+
+  })
+
+  it('O campo name deve ser obrigatório', () => {
+    const user = {
+      email: 'papito@teste.com.br',
+      password: 'pwd123'
+    }
+
+    cy.postUser(user).then((response) => {
+      expect(response.status).to.eq(400)
+
+      expect(response.body.error).to.eq('The \"name\" field is required.')
+    })
+  })
+
+  it('O campo email deve ser obrigatório', () => {
+    const user = {
+      name: 'Fernandi Papito',
+      password: 'pwd123'
+    }
+
+    cy.postUser(user).then((response) => {
+      expect(response.status).to.eq(400)
+
+      expect(response.body.error).to.eq('The \"email\" field is required.')
+    })
+  })
+
+  it('O campo senha deve ser obrigatório', () => {
+    const user = {
+      name: 'Fernandi Papito',
+      email: 'papito@teste.com.br'
+    }
+
+    cy.postUser(user).then((response) => {
+      expect(response.status).to.eq(400)
+
+      expect(response.body.error).to.eq('The \"password\" field is required.')
+    })
+  })
+
+})
+
+Cypress.Commands.add('postUser', (user) => {
+  return cy.api({
+    method: 'POST',
+    url: 'http://localhost:3333/api/users/register',
+    body: user,
+    failOnStatusCode: false
   })
 })
