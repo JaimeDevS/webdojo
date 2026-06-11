9eb7c860092f55a0253d644317f0b99a1dc18aae
JaimeDevS <jaime.dev91@gmail.com>
2026-06-10 21:20:31 -0300

    Manipulação e Interação

---
+++ web/cypress/e2e/expert.cy.js
@@ -0,0 +1,44 @@
+describe('Expert', () => {
+    beforeEach(() => {
+        cy.start()
+    })
+
+    it('Deve manipular os atributos de elementos HTML', () => {
+        cy.log('todo')
+        cy.get('#email').invoke('val', 'papito@teste.com.br')
+        cy.get('#password').invoke('attr', 'type', 'text')
+            .type('123')
+
+        cy.contains('button', 'Entrar')
+            .invoke('hide')
+            .should('not.be.visible')
+
+        cy.contains('button', 'Entrar')
+            .invoke('show')
+            .should('be.visible')
+    })
+
+    it.only('Não deve logar com senha inválida', () => {
+        cy.submitLoginForm('papito@webdojo.com', 'katana321')
+
+        //técnica para obter o HTML
+        // cy.wait(2500)
+
+        // cy.document().then((doc) => {
+        //     cy.writeFile('cypress/downloads/page.html', doc.documentElement.outerHTML)
+        // })
+
+        cy.get('[data-sonner-toaster=true]')
+            .should('be.visible')
+            .as('toast')
+
+        cy.get('@toast')
+            //.find('title')
+            .should('have.text', 'Acesso negado! Tente novamente.')
+
+        cy.wait(5000)
+
+        cy.get('@toast')
+            .should('not.exist')
+    })
+})
